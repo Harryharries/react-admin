82661e00653dff5ba2b4fae152ef66e131d6b275
HarryGu <harry.guapi@gmail.com>
2023-03-17 11:39:56 -0500

    add different table components

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,138 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
+import { useState } from "react";
+
+const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "accessLevel",
+      headerName: "Access Level",
+      headerAlign: "center",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === "admin"
+                ? colors.greenAccent[600]
+                : access === "manager"
+                ? colors.greenAccent[700]
+                : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  const onRowsSelectionHandler = (ids) => {
+    const selectedRowsData = ids.map((id) => mockDataTeam.find((row) => row.id === id));
+    setSelectedRows(selectedRowsData);
+    console.log(selectedRows);
+    console.log(selectedRowsData);
+  };
+
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box
+        m="40px 0 0 0"
+        height="70vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          checkboxSelection
+          rows={mockDataTeam}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            },
+          }}
+          pageSizeOptions={[10,25,50]}
+          keepNonExistentRowsSelected
+          onRowSelectionModelChange={(GridRowSelectedParams) => {
+            onRowsSelectionHandler(GridRowSelectedParams);
+        }}
+        />
+      </Box>
+    </Box>
+  );
+
+};
+
+export default Team;
